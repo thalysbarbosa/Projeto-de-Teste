15a001c3c7ecb9c8b85015e217894d38a3f471dd
Thalys Barbosa <thalysbarbosa23@gmail.com>
2022-09-30 17:47:04 -0300

    Janela modal feita, futuramente conectar ao server

--- js/webconf.js
+++ js/webconf.js
@@ -1,3 +1,46 @@
 window.onload = function() {
-    // código para manipulação do DOM
+  // código para manipulação do DOM
 }
+const btnRegister = document.getElementById("btnRegister")
+btnRegister.addEventListener("click", function() {
+// Abertura da janela modal
+swal({
+  title: "Inscrição na WebConference",
+  html:
+  '<input id="txtName" class="swal2-input" placeholder="name">' +
+  '<input id="txtEmail" class="swal2-input" placeholder="e-mail">',
+  showCancelButton: true,
+  confirmButtonText: "Inscrever",
+  cancelButtonText: "Cancelar",
+  showLoaderOnConfirm: true,
+  preConfirm: () => {
+    const name = document.getElementById('txtName').value
+    const email = document.getElementById('txtEmail').value
+  //   const url_base =  
+  return
+  fetch(`${url_base}/conferences/1/participants/${email}`, {
+    headers: {"Content-Type": "application/x-www-form-urlencoded" },
+    method: "POST",
+    body: `nomeparticipant=${name}`,
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();  
+  })
+  .catch(error => {
+      swal.showValidationError(`Pedido falhou: ${error}`);
+  });             
+  },
+  allowOutsideClick: () => !swal.isLoading()    
+ }) .then(result => {
+  if (result.value) {
+      if (!result.value.err_code) {
+          swal({title: "Inscrição realizada!"})
+      } else {
+          swal({title: `${result.value.err_message}`})
+      }
+   }
+ })
+})
